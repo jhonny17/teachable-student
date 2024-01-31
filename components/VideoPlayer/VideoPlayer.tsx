3f4b12896a131860caf31cae4d77c267b99c7986
Jhonny <jhonny.vargas.arias@gmail.com>
2024-01-31 12:35:23 -0600

    Improve cookie issue

--- components/VideoPlayer/VideoPlayer.tsx
+++ components/VideoPlayer/VideoPlayer.tsx
@@ -14,16 +14,7 @@ import cl from "./VideoPlayer.module.scss";
 
 const fetchPrivateVideo = async (attachmentId: number) => {
   const endpoint = `http://business-school.worksonmy.computer:3000/api/v2/hotmart/private_video?attachment_id=${attachmentId}`;
-  const response = await api(endpoint, {
-    method: "GET",
-    headers: {
-      "Content-Type": "application/json",
-      Cookie:
-        "ahoy_visitor=3c93d8fd-475c-41a8-b55b-5f145775bebf; _session_id=512ed49de783523239db805f790871bf; ajs_user_id=null; ajs_group_id=null; ajs_anonymous_id=%22e6ff48a2-a3e0-4964-9f7d-35be0adb1eeb%22; sk_ygxzxycv_access=eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiJ1c2VyIiwiaWF0IjoxNzA1MzMxOTUzLCJqdGkiOiIwYmE2Njc5YS1hOTFmLTQzOTYtYmIzMC0wZGIzYjQ0N2RiZGYiLCJpc3MiOiJza195Z3h6eHljdiIsInN1YiI6IjIzYmI5OWU1LTU0NWEtNDU2My1hNmViLTE2MGU1ZjNiNWY5YSJ9.7yqxSsaU4ofjppqOPybzNDp7a8MYtUo1kd50uAVCK4g; sk_ygxzxycv_remember_me=1; signed_in=true; __zlcmid=1JpmQKcK0DcgQFf; __stripe_mid=83da5c82-3cd9-481b-8765-aa13c8403dc3ced634; ahoy_visit=d2accff4-2b16-4428-9569-145a344d60a2; admin_csrf_token=pfoqjSfRs37DZamqRIGqpG032UQeafqVGgHbo5kZTyFrDrLob3YpALfwpl1f0EefPXHeFo7zoDNyVClWvY0G0A%3D%3D",
-      "x-csrf-token":
-        "pfoqjSfRs37DZamqRIGqpG032UQeafqVGgHbo5kZTyFrDrLob3YpALfwpl1f0EefPXHeFo7zoDNyVClWvY0G0A==",
-    },
-  });
+  const response = await api(endpoint);
 
   return { data: response } as { data?: VideoDataType };
 };
--- utils/api.ts
+++ utils/api.ts
@@ -1,9 +1,19 @@
 "use server";
 
-import { getCookie } from "./cookie";
-
 type ApiParamsType = Parameters<typeof fetch>;
 
 export const api = <T>(url: ApiParamsType[0], params?: ApiParamsType[1]) => {
-  return fetch(url, params).then((res) => res.json() as T);
+  const defaultHeaders = {
+    Cookie:
+      "ahoy_visitor=3c93d8fd-475c-41a8-b55b-5f145775bebf; _session_id=512ed49de783523239db805f790871bf; ajs_user_id=null; ajs_group_id=null; ajs_anonymous_id=%22e6ff48a2-a3e0-4964-9f7d-35be0adb1eeb%22; sk_ygxzxycv_access=eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiJ1c2VyIiwiaWF0IjoxNzA1MzMxOTUzLCJqdGkiOiIwYmE2Njc5YS1hOTFmLTQzOTYtYmIzMC0wZGIzYjQ0N2RiZGYiLCJpc3MiOiJza195Z3h6eHljdiIsInN1YiI6IjIzYmI5OWU1LTU0NWEtNDU2My1hNmViLTE2MGU1ZjNiNWY5YSJ9.7yqxSsaU4ofjppqOPybzNDp7a8MYtUo1kd50uAVCK4g; sk_ygxzxycv_remember_me=1; signed_in=true; __zlcmid=1JpmQKcK0DcgQFf; __stripe_mid=83da5c82-3cd9-481b-8765-aa13c8403dc3ced634; ahoy_visit=d2accff4-2b16-4428-9569-145a344d60a2; admin_csrf_token=pfoqjSfRs37DZamqRIGqpG032UQeafqVGgHbo5kZTyFrDrLob3YpALfwpl1f0EefPXHeFo7zoDNyVClWvY0G0A%3D%3D",
+    "x-csrf-token":
+      "pfoqjSfRs37DZamqRIGqpG032UQeafqVGgHbo5kZTyFrDrLob3YpALfwpl1f0EefPXHeFo7zoDNyVClWvY0G0A==",
+  };
+  return fetch(url, {
+    ...(params ?? {}),
+    headers: {
+      ...defaultHeaders,
+      ...(params?.headers ?? {}),
+    },
+  }).then((res) => res.json() as T);
 };
