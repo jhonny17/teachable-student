73c93cd4df7febacc2d1f65bf3ac2393a49112ca
Jhonny <jhonny.vargas.arias@gmail.com>
2024-01-31 10:47:03 -0600

    Remove unused cookie function

--- utils/api.ts
+++ utils/api.ts
@@ -5,7 +5,5 @@ import { getCookie } from "./cookie";
 type ApiParamsType = Parameters<typeof fetch>;
 
 export const api = <T>(url: ApiParamsType[0], params?: ApiParamsType[1]) => {
-  const csrfToken = decodeURIComponent(getCookie("admin_csrf_token"));
-  console.log({ csrfToken });
   return fetch(url, params).then((res) => res.json() as T);
 };
