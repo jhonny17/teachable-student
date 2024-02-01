507a599bd95c7d23f4d95b81c7b878c85444291b
katjimeno <kat.jimeno@teachable.com>
2024-01-31 18:25:34 -0700

    adds attachment name in the main content area and attachment name and anchor link to the right page navigation

--- app/courses/[courseId]/lessons/[lessonId]/page.tsx
+++ app/courses/[courseId]/lessons/[lessonId]/page.tsx
@@ -1,12 +1,13 @@
 "use client";
 
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 
 import { api } from "@/utils/api";
 import { FEDORA_HOST } from "@/utils/constants";
 import { VideoPlayer } from "@/components/VideoPlayer";
 
 import {
+  Attachment,
   LessonAttachmentsParams,
   LessonPageProps,
   ResponseAttachments,
@@ -19,25 +20,33 @@ const initAdmin = async ({ courseId, lessonId }: LessonAttachmentsParams) => {
     const data = await api<ResponseAttachments>(
       `${FEDORA_HOST}/api/v1/courses/${courseId}/lectures/${lessonId}/attachments`
     );
-
     return data.attachments;
   } catch (error) {
-    return null;
+    return [];
   }
 };
 const LecturePage = ({ params }: LessonPageProps) => {
+  const [attachmentList, setAttachmentList] = useState<Attachment[]>([]);
   useEffect(() => {
     const execute = async () => {
-      await initAdmin(params);
+      try {
+        const attachments = await initAdmin(params);
+        setAttachmentList(attachments);
+
+      } catch (error) {
+        return null;
+      }
     };
     execute();
   }, [params]);
   return (
     <div className={cl.lessonPage}>
       <div>
-        <VideoPlayer userId={-1} videoId={2} />
+        <VideoPlayer userId={-1} videoId={1} />
+        {/* TODO: Add unique ids to the mapped items */}
+        {attachmentList.map((attachment: Attachment)=> (<div id={attachment.name}>{attachment.name}</div>))}
       </div>
-      <div>attachment index</div>
+      <div>{attachmentList.map((attachment: Attachment)=> (<span><a href={'#' + attachment.name}>{attachment.name}</a></span>))}</div>
     </div>
   );
 };
