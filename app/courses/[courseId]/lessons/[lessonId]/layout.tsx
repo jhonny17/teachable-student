024761326756f6633ef71c8052bbc7cb76acb356
Jhonny <jhonny.vargas.arias@gmail.com>
2024-01-31 18:42:56 -0600

    Integrate token of uni!

--- app/courses/[courseId]/lessons/[lessonId]/layout.tsx
+++ app/courses/[courseId]/lessons/[lessonId]/layout.tsx
@@ -1,28 +1,76 @@
+"use client";
+import { useEffect, useState } from "react";
+
+import resetStyles from "@usefedora/ui/public/reset";
+import commonStyles from "@usefedora/ui/public/common";
+import tokensStyles from "@usefedora/uni/public/tokens";
+
 import { api } from "@/utils/api";
 import { FEDORA_HOST } from "@/utils/constants";
 
 import { LectureSection, RouteParams } from "./types";
 
 import cl from "./layout.module.scss";
+import { Heading, List, ListElement } from "@usefedora/ui";
 
 type LecturePageLayoutProps = {
   params: RouteParams;
   children: React.ReactNode;
 };
 
+function injectStyles(css: string): void {
+  const style = document.createElement("style");
+  style.type = "text/css";
+  style.appendChild(document.createTextNode(css));
+  document.head.appendChild(style);
+}
+
 export const getCourseLectures = async (courseId: string) => {
-  const lecture = await api<LectureSection>(
+  const lectureSections = await api<LectureSection>(
     `${FEDORA_HOST}/api/v1/courses/${courseId}/lecture_sections`
+  );
+
+  return lectureSections;
+};
+
+const LectureSectionItem = ({ section }: { section: LectureSection }) => {
+  const { name, lectures } = section;
+  return (
+    <>
+      <Heading level={3}>{name}</Heading>
+      {lectures.map((lecture) => (
+        <ListElement key={lecture.id}>{lecture.name}</ListElement>
+      ))}
+    </>
   );
 };
 
 const LecturePageLayout = ({ children, params }: LecturePageLayoutProps) => {
   const { courseId } = params;
-  getCourseLectures(courseId);
+  const [lectureSections, setLectureSections] = useState<LectureSection[]>([]);
+
+  useEffect(() => {
+    injectStyles(resetStyles);
+    injectStyles(commonStyles);
+    injectStyles(tokensStyles);
+  }, []);
+
+  useEffect(() => {
+    getCourseLectures(courseId).then(({ lecture_sections }) =>
+      setLectureSections(lecture_sections)
+    );
+  }, [courseId]);
+
   return (
     <div className={cl.lessonLayout}>
       <nav style={{ gridArea: "nav" }}>navbar</nav>
-      <aside style={{ gridArea: "sidebar" }}>aside</aside>
+      <aside className={cl.asideLessonLayout}>
+        <List className={cl.lessonList}>
+          {lectureSections.map((section) => (
+            <LectureSectionItem key={section.id} section={section} />
+          ))}
+        </List>
+      </aside>
       <main style={{ gridArea: "content" }}>content{children}</main>;
     </div>
   );
--- app/layout.tsx
+++ app/layout.tsx
@@ -1,7 +1,5 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
-import "@usefedora/ui/public/reset";
-import "@usefedora/ui/public/common";
 import "./globals.scss";
 
 const inter = Inter({ subsets: ["latin"] });
